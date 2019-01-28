4c3d22494da7fbf4ac469c9e002051796575f895
Vertigo37 <michael@menfashion.com>
2019-01-27 20:00:19 -0500

    Create user and message models

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Message.create(body: "Hey I'm a message", user: User.first)
+Message.create(body: "What's up", user: User.last)
+Message.create(body: "My ankle hurts", user: User.find_by(id: "3"))
